246d76a8dd41934ec2633ab1da1bdca44b4fac11
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-10-24 23:39:01 -0400

    Add shared context with github response.

---
+++ spec/support/shared_contexts.rb
@@ -0,0 +1,9 @@
+shared_context 'a valid github repository response' do 
+  let(:repository_response_body) do 
+    '[{"homepage":"http://rubygems.org/gems/wine_dot_com_api_request","html_url":"https://github.com/cmeiklejohn/wine_dot_com_api_request","forks":3,"language":"Ruby","svn_url":"https://svn.github.com/cmeiklejohn/wine_dot_com_api_request","watchers":3,"url":"https://api.github.com/repos/cmeiklejohn/wine_dot_com_api_request","fork":false,"updated_at":"2011-10-04T01:03:25Z","private":false,"size":240,"master_branch":null,"git_url":"git://github.com/cmeiklejohn/wine_dot_com_api_request.git","clone_url":"https://github.com/cmeiklejohn/wine_dot_com_api_request.git","description":"Provides a simple interface to cut down on code duplication when querying the wine.com API. ","owner":{"login":"cmeiklejohn","url":"https://api.github.com/users/cmeiklejohn","avatar_url":"https://secure.gravatar.com/avatar/3e09fee7b359be847ed5fa48f524a3d3?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png","gravatar_id":"3e09fee7b359be847ed5fa48f524a3d3","id":44939},"name":"wine_dot_com_api_request","pushed_at":"2010-01-02T06:46:01Z","id":455669,"ssh_url":"git@github.com:cmeiklejohn/wine_dot_com_api_request.git","open_issues":0,"created_at":"2010-01-02T03:00:05Z"}]'
+  end
+
+  let(:repository_response_headers) do 
+    {:server=>"nginx/1.0.4", :date=>"Tue, 25 Oct 2011 03:31:29 GMT", :content_type=>"application/json; charset=utf-8", :transfer_encoding=>"chunked", :connection=>"keep-alive", :status=>"200 OK", :x_ratelimit_limit=>"5000", :etag=>"\"XXX\"", :x_oauth_scopes=>"", :link=>"<https://api.github.com/users/cmeiklejohn/repos?access_token=XXX&page=2>; rel=\"next\", <https://api.github.com/users/cmeiklejohn/repos?access_token=XXX&page=2>; rel=\"last\"", :x_ratelimit_remaining=>"4534", :x_accepted_oauth_scopes=>"repo" }
+  end
+end
