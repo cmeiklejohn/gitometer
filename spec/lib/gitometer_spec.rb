2b0b4011307bf39e1a0e7ca4ebfba52738626fd0
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-10-24 22:33:08 -0400

    Add test to ensure all of the routes are responding.

--- spec/lib/gitometer_spec.rb
+++ spec/lib/gitometer_spec.rb
@@ -1,8 +1,33 @@
 require 'spec_helper'
 
-describe "Gitometer" do
-  it "should respond to /" do
+describe 'Gitometer' do
+  it 'responds to /' do
     get '/'
     last_response.should be_ok
+  end
+
+  it 'responds to /commits.json' do 
+    get '/commits.json' 
+    last_response.should be_ok
+  end
+
+  it 'responds to /login' do 
+    get '/login'
+    last_response.should be_redirect
+  end
+
+  it 'responds to /logout' do 
+    get '/logout'
+    last_response.should be_redirect
+  end
+
+  it 'responds to /auth/github/callback' do 
+    get '/auth/github/callback'
+    last_response.should be_redirect
+  end
+  
+  it 'responds to /redirect_to' do
+    get '/redirect_to'
+    last_response.should be_redirect
   end
 end
