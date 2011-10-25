ce6b5d214893700ee22cc853cfb6c42abcaa6804
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-10-24 23:52:23 -0400

    Add comments.

--- lib/gitometer/github_helpers.rb
+++ lib/gitometer/github_helpers.rb
@@ -1,17 +1,27 @@
 module Gitometer
   module GithubHelpers
+
+    # API endpoint for GitHub.
+    #
     def endpoint 
       "https://api.github.com"
     end
 
+    # Returns a query param for a users access token.
+    #
     def access_token_param_for_user(user)
       "access_token=#{user.token}"
     end
 
+    # Returns a query param for a page.
+    #
     def param_for_page(page)
       "page=#{page}" if page
     end
 
+    # Recursively finds all repositories for a particular github user by
+    # page.
+    #
     def repositories_for_user(user, page = nil)
       response = RestClient.get "#{endpoint}/users/#{user.login}/repos?#{access_token_param_for_user(user)}&#{param_for_page(page)}"
       body     = JSON.parse(response.body)
@@ -23,11 +33,15 @@ module Gitometer
       end
     end
 
+    # Find all commits for a particular repository and user.
+    #
     def commits_for_user_and_repository(user, repository)
       response = RestClient.get "#{endpoint}/repos/#{user.login}/#{repository['name']}/commits?#{access_token_param_for_user(user)}"
       JSON.parse(response.body)
     end
 
+    # Converts an array of commits to a hash of date to commit counts.
+    #
     def commits_to_daily_count(commits) 
       commits = commits.flatten.inject({}) do |h, v|
         key = Time.parse(v["commit"]["author"]["date"]).utc.strftime("%Y-%m-%d")
